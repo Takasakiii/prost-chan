453860c7fbbb7d557eea82cc9710ceb259ac80b4
BONEE4 <61917484+BONEE4@users.noreply.github.com>
2022-02-05 15:54:26 -0300

    agora o top10 funciona <:PP_Cry:835927765097250856>

--- src/Commands/nsfw/top10.js
+++ src/Commands/nsfw/top10.js
@@ -1,6 +1,7 @@
 const Command = require("../../Structures/Command")
 const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js")
-const axios = require("axios")
+const Danbooru = require("danbooru")
+const { getImage } = require('random-reddit')
 
 module.exports = class Top10Command extends Command {
     constructor(...args) {
@@ -8,12 +9,53 @@ module.exports = class Top10Command extends Command {
             name: "top10",
             category: "NSFW",
             description: "Mostra imanges NSFW/SFW de sua escolha",
-            usage: "top10 | cosplay | nekos ( SFW | NSFW ) | bikini ( SFW | NSFW ) | pussy_juice | lolicon ( Call 190 ) | yaoi | ecchi | anal | real",
+            usage: "top10 | cosplay | nekos ( SFW | NSFW ) | bikini ( SFW | NSFW ) | pussy_juice | yaoi | ecchi | anal",
             ownerOnly: false
         })
     }
 
     async run({ interaction }) {
 
+        if (interaction.options.getString("options") == "ecchi") {
+
+            let images = []
+            for (let i = 0; i < 10; i++) {
+               images.push(await getImage("ecchi"))
+            }
+    
+            let embedsArray = []
+            let i = 0
+            for (const a of images) {
+                i++
+                embedsArray.push(new MessageEmbed().setTitle(`Imagem ${i}`).setImage(a))
+            }
+    
+            return interaction.followUp({
+                content: "Aqui está suas 10 imagens <:Smirkie:939589129929367572>",
+                embeds: embedsArray
+            })
+
+        } else {
+
+            const booru = new Danbooru()
+
+            const posts = await booru.posts({ 
+                tags: interaction.options.getString("options"),
+                limit: 10
+            })
+    
+            let embedsArray = []
+    
+            let i = 0
+            for (const a of posts) {
+                i++
+                embedsArray.push(new MessageEmbed().setTitle(`Imagem ${i}`).setImage(a.file_url))
+            }
+    
+            interaction.followUp({
+                content: "Aqui está suas 10 imagens <:Smirkie:939589129929367572>",
+                embeds: embedsArray
+            })
+        }
     }
 }
